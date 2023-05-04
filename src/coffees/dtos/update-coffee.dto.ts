c181d4bddf0c426ac8e52762e17e80a1588ae369
Patryk.budnicki <p-budnicki@wp.pl>
2023-05-04 10:19:01 +0200

    feat: deploy app to railways

--- src/coffees/dtos/update-coffee.dto.ts
+++ src/coffees/dtos/update-coffee.dto.ts
@@ -1,6 +1,6 @@
 // export class UpdateCoffeeDto {
-//     readonly name?: string;
-//     readonly brand?: string;
+    // readonly name?: string;
+    // readonly brand?: string;
 //     readonly flavors?: string[];
 // }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // ValidationPipe dostarcza nam wygodny sposob 
+  // na egzekwowanie walidacji dla wszystkich
+  // przychodzących inputów za pomocą class-validatora
+  app.useGlobalPipes(new ValidationPipe({
+    // zatrzyma request jeśli jakiekolwiek dodatkowy
+    // klucz się pojawi, działa tylko jak
+    // whitelist: true jest zdefiniowane
+    forbidNonWhitelisted: true,
+    // nie prześle dodatkowych kluczy dalej
+    whitelist: true,
+  }));
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
